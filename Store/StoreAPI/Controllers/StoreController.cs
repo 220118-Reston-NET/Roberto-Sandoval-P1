ec003d45459d584d60472506035165816aaa34fe
sandovro <robertosandoval07@gmail.com>
2022-02-22 22:41:50 -0800

    Progress on logging

--- Store/StoreAPI/Controllers/StoreController.cs
+++ Store/StoreAPI/Controllers/StoreController.cs
@@ -2,6 +2,7 @@ using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using StoreBL;
 using StoreModel;
+using Serilog;
 
 namespace StoreAPI.Controllers;
 
@@ -23,11 +24,13 @@ private IStoreFrontBL _storeFrontBL;
     {
         try
         {
+            Log.Information($"User succesfully added {p_costumer.CostumerId} to datbase");
             return Created("Successfully added", _costumerBL.AddCostumer(p_costumer));
         }
-        catch (System.Exception err)
+        catch (System.Exception error)
         {
-            return Conflict(err.Message);
+            Log.Information($"Addidng costumer {p_costumer.CostumerId} resulted to exception {error.Message}");
+            return Conflict(error.Message);
         }
     }
 
@@ -36,24 +39,28 @@ private IStoreFrontBL _storeFrontBL;
     {
         try
         {
+            Log.Information($"User successfully found costumer {costumerId} in database");
             return Ok(_costumerBL.FindCostumer(costumerId));
         }
         catch (System.Exception)
         {
+            Log.Information($"Costumer {costumerId} was not found in databse");
             return NotFound();
         }
     }
 
     [HttpGet("GetAllCostumers")]
-    public IActionResult GetAllPokemon()
+    public IActionResult GetAllCostumers()
     {
         try
         {   
+            Log.Information("User successfully got list of costumers");
             return Ok(_costumerBL.GetAllCostumers());
         }
-        catch (SqlException)
+        catch (SqlException error)
         {
-            return NotFound();
+            Log.Information($"GetAllCostumers exited with SQL exception {error.Message}");
+            return Conflict(error.Message);
         }
     }
 
@@ -62,11 +69,13 @@ private IStoreFrontBL _storeFrontBL;
     {
         try
         {
+            Log.Information($"User successfully got order history for costumer {costumerId}");
             return Ok(_costumerBL.orderHistory(costumerId));
         }
-        catch (System.Exception)
+        catch (System.Exception error)
         {
-            return NotFound();
+            Log.Information($"Get order history for costumer {costumerId} exited with exception {error.Message}");
+            return Conflict(error.Message);
         }
     }
 
@@ -75,11 +84,13 @@ private IStoreFrontBL _storeFrontBL;
     {
         try
         {
+            Log.Information($"User successfully got inventory for store {storeNumber}");
             return Ok(_storeFrontBL.listInventory(storeNumber));
         }
-        catch (System.Exception)
+        catch (System.Exception error)
         {
-            return NotFound();
+            Log.Information($"Get inventory for store {storeNumber} exited with exception {error.Message}");
+            return Conflict(error.Message);
         }
     }
 
@@ -88,11 +99,13 @@ private IStoreFrontBL _storeFrontBL;
     {
         try
         {
+            Log.Information($"Manager {managerId} successfully added stock to inventory for store {inventoryList[0].StoreNumber}");
             return Created("Successfully added", _storeFrontBL.addInventory(inventoryList, managerId, ManagerPassword));
         }
-        catch (System.Exception err)
+        catch (System.Exception error)
         {
-            return Conflict(err.Message);
+            Log.Information($"Replenish order from {managerId} for store {inventoryList[0].StoreNumber} exited with exception {error.Message}");
+            return Conflict(error.Message);
         }
     }
 
@@ -101,11 +114,13 @@ private IStoreFrontBL _storeFrontBL;
     {
         try
         {
+            Log.Information($"Order for costumer {orderCostumer.CostumerId} placed successfully from store {orderStoreNumber}");
             return Created("Successfully processed order for costumer: ", _costumerBL.processOrder(orderProducts, orderCostumer, orderStoreNumber));
         }
-        catch (System.Exception err)
+        catch (System.Exception error)
         {
-            return Conflict(err.Message);
+            Log.Information($"Order for costumer {orderCostumer.CostumerId} exited with exception {error.Message}");
+            return Conflict(error.Message);
         }
     }
 
@@ -125,22 +140,22 @@ private IStoreFrontBL _storeFrontBL;
     //     return "value";
     // }
 
-    // POST: api/Store
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-    }
+    // // POST: api/Store
+    // [HttpPost]
+    // public void Post([FromBody] string value)
+    // {
+    // }
 
-    // PUT: api/Store/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
+    // // PUT: api/Store/5
+    // [HttpPut("{id}")]
+    // public void Put(int id, [FromBody] string value)
+    // {
+    // }
 
-    // DELETE: api/Store/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
+    // // DELETE: api/Store/5
+    // [HttpDelete("{id}")]
+    // public void Delete(int id)
+    // {
+    // }
 }
 
